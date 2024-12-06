421d85d7bf4579350c1c341d60892421b6dfefdd
Chidi Nkwocha <chidi.nkwocha@rayda.co>
2024-12-06 12:27:34 +0100

    Set application key

--- start.sh
+++ start.sh
@@ -3,5 +3,11 @@
 # Navigate to the root directory (if not already there)
 cd "$(dirname "$0")"
 
+# Navigate to the backend directory
+cd /usr/src/app/backend
+
+# Run migrations and set application key
+php artisan key:generate
+
 # Build and start the application fresh
 docker-compose up --build
